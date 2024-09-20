e7b97515c5cd7e8a7d23cc2938fa761b5e89ac40
lucasformento <lucasformento6256@gmail.com>
2024-09-20 10:15:02 -0300

    Primer commit

---
+++ wtf/src/main/java/uy/edu/um/wtf/WtfApplication.java
@@ -0,0 +1,13 @@
+package uy.edu.um.wtf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WtfApplication.class, args);
+	}
+
+}
---
+++ wtf/src/test/java/uy/edu/um/wtf/WtfApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.edu.um.wtf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
