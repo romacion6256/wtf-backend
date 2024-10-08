6e28d452d4e7ccf3b9e48fb3f461b4a7a2ad4a34
antosacco <127422866+antosacco@users.noreply.github.com>
2024-10-08 15:39:01 -0300

    Cambios para habilitar Google Cloud

--- wtf/src/main/java/uy/edu/um/wtf/WtfApplication.java
+++ wtf/src/main/java/uy/edu/um/wtf/WtfApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 
 public class WtfApplication {
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(WtfApplication.class, args);
+		}
 	}
-
-}
--- wtf/src/main/java/uy/edu/um/wtf/config/JpaConfiguration.java
+++ wtf/src/main/java/uy/edu/um/wtf/config/JpaConfiguration.java
@@ -4,25 +4,39 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 import jakarta.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = {"uy.edu.um"})
-public class JpaConfiguration
-{
+
+public class JpaConfiguration {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("uy.edu.um.wtf.entities");
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        em.setJpaProperties(hibernateProperties());
+        return em;
+    }
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+        return transactionManager;
+    }
     @Bean
     public PlatformTransactionManager transactionManager()
     {
@@ -61,7 +75,15 @@ public class JpaConfiguration
         hibernateProp.put("hibernate.hbm2ddl.auto", "update");
         return hibernateProp;
     }
-
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://34.95.128.190:5432/postgres")  // Replace with your PostgreSQL or MySQL details
+                .username("postgres")  // Your database username
+                .password("grupo6")  // Your database password
+                .driverClassName("org.postgresql.Driver")  // Change to MySQL driver if needed
+                .build();
+    }
     /*@Bean
     public DataSource dataSource()
     {
